30fb8cc77c5b6dcca260f8c581c54e26a8aec336
tijesunimi-5 <tijesunimiidowu16@gmail.com>
2024-10-13 15:17:33 +0100

    first commit

--- app/page.js
+++ app/page.js
@@ -2,100 +2,77 @@ import Image from "next/image";
 
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-8 row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-semibold">
-              app/page.js
-            </code>
-            .
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:min-w-44"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="text-center ">
+      <div className="relative">
+        <div className="relative top-[-80px]">
+          <img src="/heroImage.jpg" className="hero" />
+          <div className="overlay"></div>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-6 flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="relative z-20 text-white">
+          <h1 className=" uppercase mt-20 font-bold">
+            Welcome To Your Exam
+          </h1>
+          <p className="text-start px-2">
+            Please read your answers through before clicking the submit button.
+            You can only submit the exam once. Any other submissions will be
+            disregarded. <br />
+            Please give your correct contact details as indicated in your
+            registration form.
+          </p>
+        </div>
+      </div>
+
+      <div className="mt-10">
+        <h1 className="font-bold">Fill in correct details</h1>
+        <form className="mt-3">
+          <div className="text-start ml-9">
+            <label htmlFor="fullname" className=" pl-2 font-semibold">
+              Full name:
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="border-2 border-black w-[300px] rounded-lg"
+            />
+          </div>
+
+          <div className="mt-3 text-start ml-9">
+            <label htmlFor="email" className=" pl-2 font-semibold">
+              Email:
+            </label>
+            <input
+              type="email"
+              id="email"
+              className="border-2 border-black w-[300px] rounded-lg"
+            />
+          </div>
+
+          <div className="mt-3 text-start ml-9">
+            <label htmlFor="phone-number" className=" pl-2 font-semibold">
+              Phone number:
+            </label>
+            <input
+              type="number"
+              id="phone-number"
+              className="border-2 border-black w-[300px] rounded-lg"
+            />
+          </div>
+
+          <div className="mt-3 text-start ml-9">
+            <label htmlFor="fullname" className="text-start pl-2 font-semibold">
+              Name of course:
+            </label>
+            <input
+              type="text"
+              id="name"
+              className="border-2 border-black w-[300px] rounded-lg"
+            />
+          </div>
+          <button className="mt-4 border-2 border-black rounded-md w-[200px] py-1">
+            Submit
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+  return (
+    <header className='fixed top-0 right-0 left-0 text-center h-8 bg-orange-400 text-white z-30'>
+      <h1 className='mt-1 text-2xl font-bold'>RCCG fountain of God's glory</h1>
+    </header>
+  )
+}
+
+export default Header
